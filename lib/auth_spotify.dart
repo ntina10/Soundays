9bf45b3fe91c010fd06f30e47838437b5e821678
ntina10 <frenadin@hotmail.com>
2023-02-17 20:42:52 +0200

    details

--- lib/auth_spotify.dart
+++ lib/auth_spotify.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
-import 'package:soundays/credentials.dart' as creds;
+import 'package:soundays/creds.dart' as creds;
 
 class Authentication {
   final String url = "https://accounts.spotify.com/api/token";
